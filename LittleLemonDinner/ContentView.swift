07c3e4969ca7dc3a794a23ac80bbb019a67a0abe
oluwasegun <ilorioluwasegun@gmail.com>
2024-08-07 10:36:39 -0400

    Initial Commit

---
+++ LittleLemonDinner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleLemonDinner
+//
+//  Created by shegz on 07/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LittleLemonDinner/LittleLemonDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerApp.swift
+//  LittleLemonDinner
+//
+//  Created by shegz on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
